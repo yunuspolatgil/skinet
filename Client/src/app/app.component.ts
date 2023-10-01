bbaabc8675258a3b3a1bf3c1cc7196906c3cb696
Yunus Polatgil <polatgil@mail.com>
2023-10-01 23:19:29 +0300

    Bölüm 8:Client-Angular Basics

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Product} from "./models/product";
+import {Pagination} from "./models/pagination";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Client';
+  products: Product[]=[]
+  constructor(private http:HttpClient) {
+  }
+
+  ngOnInit(): void {
+        this.http.get<Pagination<Product[]>>("https://localhost:5001/api/products?pageSize=50").subscribe({
+          next: response => this.products = response.data, //What to do next
+          error: err => console.log(err),//What to do if there is an error
+          complete: () => {
+            console.log("request complated");
+            console.log("extra statment")
+          }
+        });
+    }
 }
--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
   import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
